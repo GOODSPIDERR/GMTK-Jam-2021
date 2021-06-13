df6cd8cff42c608d44242da400719762e89113b0
johnhanson <9466237+johnhanson@users.noreply.github.com>
2021-06-12 20:35:43 -0700

    More work on Health systems and ball behavior stuff

--- GMTK Jam 2021/Assets/Scripts/BallBehavior.cs
+++ GMTK Jam 2021/Assets/Scripts/BallBehavior.cs
@@ -8,15 +8,60 @@ public class BallBehavior : MonoBehaviour
     public Vector3 velocity;
     public Rigidbody rigidBody;
     
+    // the amount of damage (speed) needed to destory an object
+    public float thresholdForBreakingObjects;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        
+        // we immediatly squre the threshold because we are using Vector3 sqrMagnitude to compare
+        // because it's faster
+        thresholdForBreakingObjects = thresholdForBreakingObjects * thresholdForBreakingObjects;
     }
 
     // Update is called once per frame
     void Update()
     {
         velocity = rigidBody.velocity;
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        int numContacts = other.contactCount;
+        Debug.Log("there are " + numContacts + " contact points!");
+        Collider objectCollider, ballCollider;
+        float ballVelocitySqr;
+        
+        for (int i = 0; i < numContacts; i++) {
+            objectCollider = other.GetContact(i).thisCollider;
+            ballCollider = other.GetContact(i).thisCollider;
+            ballVelocitySqr = ballCollider.attachedRigidbody.velocity.sqrMagnitude;
+            
+            Debug.Log("objectCollider.tag: " + objectCollider.tag);
+            Debug.Log("ballCollider.tag: " + ballCollider.tag);
+            
+            if (objectCollider.tag == "BreakableObject") // check for breakable object
+            {
+                if (ballVelocitySqr > thresholdForBreakingObjects) 
+                {
+                    objectCollider.GetComponent<Break>().isBroken = true;
+                }
+            }
+            else if (objectCollider.tag == "Enemy") // check for enemy
+            {
+                objectCollider.GetComponent<EnemyHealth>().tryToDamage(Mathf.Sqrt(ballVelocitySqr));
+            }
+            else if (objectCollider.tag == "Bullet") // check for bullets
+            {
+                
+            }
+            else if (objectCollider.tag == "Player") // check for the player
+            {
+                
+            }
+        }
     }
 }
