fe53528ce6c17c5a5828ecd11c0823ef11b0a41a
GrFlower <greesem99@gmail.com>
2021-06-13 04:56:08 -0700

    Revert "Merge branch 'main' of https://github.com/OffworldDemon/GMTK-Jam-2021 into main"
    This reverts commit f770ddcb2654ac74b8418a0c73fbf7f3cdb293b4, reversing
    changes made to 7fa4b543ca1c4c1f2fc212c7977c0864d584d8cf.

--- GMTK Jam 2021/Assets/Scripts/FinalDoorScript.cs
+++ GMTK Jam 2021/Assets/Scripts/FinalDoorScript.cs
@@ -5,38 +5,10 @@ using UnityEngine;
 public class FinalDoorScript : MonoBehaviour
 {
     public GameObject[] keys;
-    [SerializeField] VladMovement playerScript;
-    [SerializeField] GameObject needMoreKeys;
-
     void Start()
     {
         keys = GameObject.FindGameObjectsWithTag("Key");
-        playerScript = FindObjectOfType<VladMovement>();
 
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (playerScript.keyCounter == 5)
-        {
-            Destroy(gameObject);
-        }
-        if (playerScript.keyCounter < 5)
-        {
-            StartCoroutine(NotEnoughKeys(3));
-        }
-    }
-
-
-    IEnumerator NotEnoughKeys(float seconds)
-    {
-        float counter = seconds;
-        while (counter > 0)
-        {
-            yield return new WaitForSeconds(1);
-            needMoreKeys.SetActive(true);
-            counter--;
-        }
-        needMoreKeys.SetActive(false);
     }
 
     // Update is called once per frame
