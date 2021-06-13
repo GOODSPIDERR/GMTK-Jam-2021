2ce628d46c8fa39f461236df914f35f2f067f97f
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2021-06-13 10:48:52 -0400

    just put this on doors and they will break

--- GMTK Jam 2021/Assets/Scripts/DoorDestruction.cs
+++ GMTK Jam 2021/Assets/Scripts/DoorDestruction.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class DoorDestruction : MonoBehaviour
 {
     public GameObject wallBreak;
+    public float breakAngle = 135f;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Ball")
         {
-            Instantiate(wallBreak, transform.position, transform.rotation);
+            Instantiate(wallBreak, transform.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y - breakAngle, transform.rotation.z));
             Destroy(gameObject);
         }
     }
