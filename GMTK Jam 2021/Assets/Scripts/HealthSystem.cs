71f66fe16ba57cc2f0280abccb61e2693cab780b
johnhanson <9466237+johnhanson@users.noreply.github.com>
2021-06-13 08:49:51 -0700

    More attempts to update the health system

--- GMTK Jam 2021/Assets/Scripts/HealthSystem.cs
+++ GMTK Jam 2021/Assets/Scripts/HealthSystem.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+using DG.Tweening;
 
 public class HealthSystem : MonoBehaviour
 {
@@ -8,18 +11,27 @@ public class HealthSystem : MonoBehaviour
     public float curHealth;
     
     // HP per second
-    public float regenSpeed;
+    public float regenSpeed = 2f;
     
-    public bool isAttached = true;
+    public bool shouldRegenHP = true;
     
+    public float lastHit;
+    public float invlunTime;
+    
+    public Image HPBar;
+    
+    CinemachineVirtualCamera mainCamera;
+
+    public AudioSource ouch;
+    public AudioSource dying;
     
     // Start is called before the first frame update
     void Start()
     {
-        if (maxHealth != curHealth)
-        {
-            curHealth = maxHealth;
-        }
+        ouch = FindObjectOfType<AudioSource>(CompareTag("PlayerOuch"));
+        dying = FindObjectOfType<AudioSource>(CompareTag("PlayerDying"));
+        mainCamera = GameObject.FindGameObjectWithTag("MainCameraCM").GetComponent<CinemachineVirtualCamera>();
+
     }
 
     // Update is called once per frame
@@ -28,12 +40,29 @@ public class HealthSystem : MonoBehaviour
         TryHealing();
     }
     
+    public void tryToDamage(float damage)
+    {
+        if (Time.time > lastHit + invlunTime)
+        {
+            lastHit = Time.time;
+            if (curHealth - damage < 0)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                curHealth -= damage;
+            }
+        }
+    }
+    
     void TryHealing()
     {
-        if (!isAttached && curHealth < maxHealth)
+        if (Time.time > lastHit + invlunTime && curHealth < maxHealth)
         {
             // increase health dependant on the time it takes to render a frame
             curHealth += regenSpeed * Time.deltaTime;
+            UpdateUIHealth();
             if (curHealth > maxHealth) // don't overheal
             {
                 curHealth = maxHealth;
@@ -41,4 +70,32 @@ public class HealthSystem : MonoBehaviour
         }
     }
     
+    void UpdateUIHealth()
+    {
+        HPBar.fillAmount = curHealth / maxHealth;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Guard" && Time.time > lastHit + invlunTime)
+        {
+            Debug.Log("Player hit guard!");
+            Rigidbody otherRb = other.GetComponent<Rigidbody>();
+            curHealth -= 5;
+            UpdateUIHealth();
+
+            Vector3 direction = transform.position - other.transform.position;
+            otherRb.AddForce(-direction * 30f, ForceMode.VelocityChange);
+
+            ShakeCamera(otherRb.velocity.magnitude / 2, 0.5f);
+        }
+    }
+
+    public void ShakeCamera(float intensity, float timer)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = mainCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        DOTween.To(() => cinemachineBasicMultiChannelPerlin.m_AmplitudeGain, x => cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = x, 0f, timer);
+    }
 }
