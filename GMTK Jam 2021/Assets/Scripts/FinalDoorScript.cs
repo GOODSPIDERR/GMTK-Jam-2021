a95209176def65630a5a6f652573ee59767bd9d6
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2021-06-20 02:58:05 -0400

    final door tweaks

--- GMTK Jam 2021/Assets/Scripts/FinalDoorScript.cs
+++ GMTK Jam 2021/Assets/Scripts/FinalDoorScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class FinalDoorScript : MonoBehaviour
 {
@@ -13,9 +14,14 @@ public class FinalDoorScript : MonoBehaviour
     [SerializeField] AudioSource backgroundMusic;
     public AudioClip endSong;
     private Animator anim;
+    public Material material;
+    float materialOpacity = 1f;
+    BoxCollider boxCollider;
+    public MeshCollider doorMeshCollider;
 
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider>();
         keys = GameObject.FindGameObjectsWithTag("Key"); //locats all keys and puts them in array
         playerScript = FindObjectOfType<VladMovement>(); //ref to player script
         soundEffects = FindObjectOfType<SoundEffectManagerScript>(); //ref to sound manager 
@@ -27,14 +33,17 @@ public class FinalDoorScript : MonoBehaviour
     {
         if (other.transform.tag == "Player") //If player touches the door
         {
-            if (playerScript.keyCounter >= 5) 
+            if (playerScript.keyCounter >= 5)
             {
+                boxCollider.enabled = false;
+                doorMeshCollider.enabled = false;
                 soundEffects.FinalDoorBreak(); //from sound effect manager
                 backgroundMusic.clip = endSong;
                 backgroundMusic.Play();
                 anim.SetTrigger("doorOpen");
                 StartCoroutine(DoorOpen(3)); //sets unlocked door UI to active for 3 secs
-                Destroy(gameObject, 2.8f); //makes the door disappear
+                Destroy(gameObject, 5f); //makes the door disappear
+                StartCoroutine(DoorFade(2.8f));
             }
             if (playerScript.keyCounter < 5)
             {
@@ -60,14 +69,33 @@ public class FinalDoorScript : MonoBehaviour
     IEnumerator DoorOpen(float seconds)
     {
         float counter = seconds;
+
         while (counter > 0)
+
         {
             yield return new WaitForSeconds(1);
             doorOpen.SetActive(true); //displays UI 
 
+
             counter--;
         }
+
+
+
+
         doorOpen.SetActive(false); //turns it back off 
+    }
+
+    IEnumerator DoorFade(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        DOTween.To(() => materialOpacity, x => materialOpacity = x, 0f, 2.2f).SetEase(Ease.Linear);
+    }
+
+    private void Update()
+    {
+        Debug.Log(materialOpacity);
+        material.SetFloat("Vector1_b33ae070a08446a09975bc7e8bbc640c", materialOpacity);
     }
     /*public void CheckForSuccess() //I'll fix this later
     {
@@ -90,4 +118,4 @@ public class FinalDoorScript : MonoBehaviour
         
     }*/
 
-} 
+}
